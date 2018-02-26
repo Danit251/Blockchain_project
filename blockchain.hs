877680312b0a4d580037532cd4685afafc56bd66
Roi Shtivi <roi1255@cs.huji.ac.il>
2018-02-26 12:24:38 +0200

    kinda working

--- blockchain.hs
+++ blockchain.hs
@@ -3,15 +3,15 @@ import Numeric
 import Data.Maybe
 
 a = createGenesisBlock
-m = 2^253
+m = 2^253 :: Integer
 
-zero = Block {index = 0, text = "ZERO", prevHash = "0", hash = ""}
-one = Block {index = 1, text = "ONE", prevHash = "0", hash = ""}
+zeroBlock = (Block {index = 0, text = "ZERO", prevHash = "0", hash = "0"})
+zero = BlockChain zeroBlock []
+one = BlockChain (Block {index = 1, text = "ONE", prevHash = "0", hash = ""}) []
+
+
 two = Block {index = 2, text = "TWO", prevHash = "0", hash = ""}
 three = BlockChain (Block {index = 3, text = "THREE", prevHash = "0", hash = ""}) []
---retree = BlockChain 5 [BlockChain 3 [BlockChain 1 [], BlockChain 4[]], BlockChain 7 []]
-
-rosetree = BlockChain zero [BlockChain one [], BlockChain two []]
 
 data BlockChain a = BlockChain a [BlockChain a] deriving (Show)
 
@@ -24,15 +24,16 @@ data Block = Block {index :: Int,
                     } deriving (Show) 
                     
                     
-numericHash hash = fst $ head $ readHex hash
+numericHash hash = (fst $ head $ readHex hash) :: Integer
 
 createGenesisBlock = Block {index = 0, text = "This is the first text", prevHash = "0", hash = (sha256 "0This is the first text0")}
 
 mineBlock prevBlock newText nonce 
                                  |(numericHash currHash) > m = mineBlock prevBlock newText (nonce + 1)
-                                 |otherwise = createBlock prevBlock newText currHash
+                                 |otherwise = (createBlock prevBlock newText currHash, seed)
                                  where
-                                   currHash =  sha256 ((show ((index prevBlock) + 1)) ++ newText ++ (hash prevBlock) ++ (show nonce))
+                                   seed = (show ((index prevBlock) + 1)) ++ newText ++ (hash prevBlock) ++ (show nonce)
+                                   currHash = sha256 (seed)
 
                                    
 createBlock prevBlock newText blockHash = Block {index = (index prevBlock) + 1,
@@ -40,14 +41,14 @@ createBlock prevBlock newText blockHash = Block {index = (index prevBlock) + 1,
                                        prevHash = hash prevBlock, 
                                        hash = blockHash}
                                        
---try2AddBlock blockChain block prevId seed = if (sha256 seed) < m 
---                             then addBlock blockChain block prevId -- "Just newBlockChain"
---                             else blockChain -- IMPORTENT not the real return value should be "Nothing"
+try2AddBlock bc b prevId seed = if (numericHash $ sha256 seed) < m 
+                                then addBlock prevId bc (BlockChain b []) 
+                                else bc
 
 
-add id (BlockChain x []) _
+addBlock id (BlockChain x []) _
              |(index x) /= id = (BlockChain x []) 
-add id (BlockChain x ys) block 
+addBlock id (BlockChain x ys) block 
                           |(index x == id) = BlockChain x $ ys ++ [block]
-                          |otherwise = BlockChain x $ map (\z -> add id z block) ys
+                          |otherwise = BlockChain x $ map (\z -> addBlock id z block) ys
 
